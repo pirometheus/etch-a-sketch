69ececbf043336a77ceabbb0b00c63429c8dde3b
pirometheus <pirsu.turan@gmail.com>
2024-10-09 00:02:10 +0200

    creating n times div element

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+
+
+for (let i=0;i<=8;i++){
+    var box = document.createElement("div");
+    box.className = `box`;
+    container.appendChild(box);
+}
+
