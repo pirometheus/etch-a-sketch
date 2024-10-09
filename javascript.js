ce01140943d8575ada4baacefb6ca0956cd83942
pirometheus <pirsu.turan@gmail.com>
2024-10-09 23:44:25 +0200

    grid problem solved

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 const container = document.querySelector(".container");
 
+let initialNumber=16;
+let side=  (800)/initialNumber+ "px";
 
-for (let i=0;i<=8;i++){
+for (let i=0;i<=(initialNumber**2)-1;i++){
     var box = document.createElement("div");
     box.className = `box`;
+    box.style.minWidth = side;
+    box.style.minHeight = side;
     container.appendChild(box);
+   console.log(i)
 }
 
