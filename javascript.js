fc79d3a0903534b75b5b7874246fa912d5522ae5
pirometheus <pirsu.turan@gmail.com>
2024-10-10 21:10:36 +0200

    hovering divs done

--- javascript.js
+++ javascript.js
@@ -1,14 +1,42 @@
 const container = document.querySelector(".container");
+const body = document.querySelector("body");
 
-let initialNumber=16;
+let initialNumber=3;
 let side=  (800)/initialNumber+ "px";
 
 for (let i=0;i<=(initialNumber**2)-1;i++){
     var box = document.createElement("div");
-    box.className = `box`;
-    box.style.minWidth = side;
-    box.style.minHeight = side;
-    container.appendChild(box);
-   console.log(i)
+    box.className = "box";
+    box.style.width = side;
+    box.style.height = side;
+    container.appendChild(box);        
 }
 
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(box => {
+    box.addEventListener("mouseover", () => {
+        // Add your desired functionality here
+        box.style.backgroundColor = "lightblue"; // Example: Change background color
+    });
+});
+
+boxes.forEach(box => {
+    box.addEventListener("mouseout", () => {
+        // Add your desired functionality here
+        box.style.backgroundColor = "rgb(241, 241, 177)"; // Example: Change background color
+    });
+});
+
+// This is my test div
+const div = document.createElement("div"); 
+div.style.border= "1px solid black";
+div.style.padding= "50px";
+body.appendChild(div)
+div.addEventListener("mouseover",()=>{
+    div.style.backgroundColor= "red";
+});
+
+div.addEventListener("mouseout",()=>{
+    div.style.backgroundColor= " rgb(241, 241, 177)";
+})
