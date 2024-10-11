19642ca273323a6fb565e3ebd9cafd03af2bb762
pirometheus <pirsu.turan@gmail.com>
2024-10-11 23:34:47 +0200

    user input added

--- javascript.js
+++ javascript.js
@@ -1,49 +1,82 @@
 const container = document.querySelector(".container");
 const body = document.querySelector("body");
+const submit = document.querySelector(".submit");
+const getInput = document.getElementById("getInput");
+const reset = document.querySelector(".reset");
+const userInput = document.querySelector(".userInput");
 
-let initialNumber=25;
+
+let initialNumber =16;
 let side=  (800)/initialNumber+ "px";
 
-for (let i=0;i<=(initialNumber**2)-1;i++){
-    var box = document.createElement("div");
-    box.className = "box";
-    box.style.width = side;
-    box.style.height = side;
-    container.appendChild(box);        
-}
 
-const boxes = document.querySelectorAll(".box");
-
-boxes.forEach(box => {
-    let isClicked = false;
-
-    box.addEventListener("mouseover", () => {
-        if (!isClicked) {
-            box.style.backgroundColor = "black";   
-        }
-    });
-
-    //First click makes the box black
-    box.addEventListener("mouseout", () => {
-        if (!isClicked) {
-            box.style.backgroundColor = "rgb(241, 241, 177)";
-        }
-    });
+function createBoxes() {
+    container.innerHTML = "";
     
-    // Second click makes it yellow again
-    let clickCount = 0;
-    box.addEventListener("click", () => {
-        isClicked = true;
-        if (clickCount % 2){
-            box.style.backgroundColor = "rgb(241, 241, 177)";
-        } else {
-            box.style.backgroundColor = "black";
-        }
-        clickCount++;
-    });
+    for (let i=0;i<=(initialNumber**2)-1;i++){
+        var box = document.createElement("div");
+        box.className = "box";
+        box.style.width = side;
+        box.style.height = side;
+        container.appendChild(box);        
+    }
 
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach(box => {
+        let isClicked = false;
     
+        box.addEventListener("mouseover", () => {
+            if (!isClicked) {
+                box.style.backgroundColor = "black";   
+            }
+        });
+    
+        //First click makes the box black
+        box.addEventListener("mouseout", () => {
+            if (!isClicked) {
+                box.style.backgroundColor = "rgb(241, 241, 177)";
+            }
+        });
+        
+        // Second click makes it yellow again
+        let clickCount = 0;
+        box.addEventListener("click", () => {
+            isClicked = true;
+            if (clickCount % 2){
+                box.style.backgroundColor = "rgb(241, 241, 177)";
+            } else {
+                box.style.backgroundColor = "black";
+            }
+            clickCount++;
+        });    
+    });
+};
+
+createBoxes();
+
+submit.addEventListener("click", ()=>{
+    initialNumber = document.getElementById("getInput").value;
+    side=  (800)/initialNumber+ "px";
+    getInput.value="";
+    userInput.textContent = `Grid Size: ${initialNumber}x${initialNumber}`;
+
+    createBoxes();
 });
+
+reset.addEventListener("click",()=>{
+    createBoxes();
+});
+
+
+
+
+
+
+
+
+
+
 
 
 // This is my test div
